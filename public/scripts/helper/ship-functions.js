5c90d3e278c567eb0d796a6d4294b122f01fa829
byeong0430 <byeong.kim0430@gmail.com>
2018-06-22 06:36:34 +0000

    1. created new ship-functions. 2. out-of-bound casees will be shown in red. 3. dblclick changes ship orientation

--- public/scripts/helper/ship-functions.js
+++ public/scripts/helper/ship-functions.js
@@ -27,7 +27,51 @@ const selectCells = (firstCell, len, coord, dim) => {
   return result;
 };
 
+const showShipLocation = (shipSize, cellLine) => {
+  const className = (shipSize !== cellLine.length) ? 'warning' : 'ok';
+
+  // first, remove 'warning' and 'ok' from <td> class  
+  $('#my-board td').removeClass('warning ok');
+  // then add appropriate class name to newly selected cells
+  cellLine.forEach(cell => {
+    $(`#${cell}`).addClass(className);
+  });
+};
+
+const getLocation = (event, numClicks, shipSize, tableDim) => {
+  // get the cell id
+  // if you click on something other than <td>, targetCell becomes an empty value. in this case, stop executing subsequent code
+  const shipHeadCell = event.target.id;
+  if (!shipHeadCell) return;
+
+  // if numClicks is odd, set coordination to horizontal. otherwise vertical
+  // this allows us to alternate the coordinates back and forth
+  const coord = (numClicks % 2) ? 'h' : 'v';
+  return selectCells(shipHeadCell, shipSize, coord, tableDim);
+
+  // console.log(`cell head: ${shipHeadCell}`, coord, `length: ${shipSize}`, selectedCells);
+};
+
+const showWarning = (shipSize, cellLine) => {
+  // if ship length !== selectedCells.length, this is because ship is outside the table range
+  // in this case, create an alert
+  const warning = (shipSize !== cellLine.length) ? 'ship out of bound' : '';
+
+  const tooltip = $('span.tooltip').text(warning);
+  const x = event.clientX;
+  const y = event.clientY;
+  tooltip.css('top', `${y - 25}px`);
+  tooltip.css('left', `${x}px`);
+};
+
+// place ship on the table
+const placeShip = (mouseE, clickNum, shipLength, tblDim) => {
+  const selectedCells = getLocation(mouseE, clickNum, shipLength, tblDim);
+  showShipLocation(shipLength, selectedCells);
+  showWarning(shipLength, selectedCells);
+};
+
 // module is undefined in the browser in client-side JS
 // associate whatever gets exported to the window scope
 // reference: https://www.matteoagosti.com/blog/2013/02/24/writing-javascript-modules-for-both-browser-and-node/
-window.selectCells = selectCells;
+window.placeShip = placeShip;
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,46 +1,24 @@
-$(document).ready(function () {
+$(() => {
+  const { createBoard } = tableFunc;
+
   // define the board dimensions
   const tblDim = { width: 10, height: 10 };
   // create a board
-  const $board = tableFunc.createBoard(tblDim);
+  const $board = createBoard(tblDim);
   // append the board to section#my-board
   $('#my-board').append($board);
 
-  // on mouse click
+
   let clickCount = 0;
-  $('#my-board').on('click', (event) => {
+  const shipLength = 5;
+  // on double-click, change ship orientation
+  $('#my-board').on('dblclick', e => {
     clickCount++;
-    // get the cell id
-    // if you click on something other than <td>, targetCell becomes an empty value. 
-    // in this case, stop executing subsequent code
-    const targetCell = event.target.id;
-    if (!targetCell) return;
+    placeShip(e, clickCount, shipLength, tblDim);
+  })
 
-    // if clickCount is odd, set coordination to horizontal. otherwise vertical
-    // this allows us to alternate the coordinates back and forth
-    const coord = (clickCount % 2) ? 'h' : 'v';
-
-    // select cells by given length
-    const shipLength = 5;
-    const selectedCells = selectCells(targetCell, shipLength, coord, tblDim);
-
-    // if ship length !== selectedCells.length, this is because ship is outside the table range
-    // in this case, create an alert
-    const warning = (shipLength !== selectedCells.length) ? 'ship out of bound' : '';
-    console.log(`cell head: ${targetCell}`, coord, `length: ${shipLength}`, warning, selectedCells);
-
-    const tooltip = $('span.tooltip').text(warning);
-    const x = event.clientX;
-    const y = event.clientY;
-    tooltip.css('top', `${y - 25}px`);
-    tooltip.css('left', `${x}px`);
-
-
-    // first, remove class from <td>s with class='selected'
-    $('td[class="selected"]').removeClass('selected');
-    // then add class='selected' to newly selected cells
-    selectedCells.forEach(cell => {
-      $(`#${cell}`).addClass('selected');
-    });
+  // on click, show ship location
+  $('#my-board').on('click', e => {
+    placeShip(e, clickCount, shipLength, tblDim);
   })
 });
